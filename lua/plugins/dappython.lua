cc8a532a7e4f0230954306b20d9e0c32668e03aa
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-20 23:25:24 -0400

    FIX: readded correct dap-python configuration

--- lua/plugins/dappython.lua
+++ lua/plugins/dappython.lua
@@ -1,6 +1,15 @@
 return {
   "mfussenegger/nvim-dap-python",
   config = function()
-    require("dap-python").setup("C:\\Users\\frank\\AppData\\Local\\Programs\\Python\\Python312\\python.exe")
-  end,
+		local executable
+		executable = vim.fn.exepath("python3")
+		if executable == "" then
+			executable = vim.fn.exepath("python")
+		end
+
+		if executable == "" then
+			error("Could not find python executable for dap-python", 0)
+		end
+		require("dap-python").setup(executable)
+	end,
 }
