7d12a1f24cef86ddc98d1e8d6be542149e057d84
Franklyn Vasquez <franklynvasquez24@gmail.com>
2026-03-23 23:39:26 -0400

    FIX: readded custom keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,32 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Use mbbill's undotree since that's an actual tree
+vim.keymap.set({ 'n', 'i', }, "<leader>su", vim.cmd.UndotreeToggle, {
+  desc = "Undotree (real tree)"
+})
+
+-- Find help for visual selected
+vim.keymap.set({ "v" }, "gh", function()
+  local visual_selected = vim.fn.getregion(
+    vim.fn.getpos("."),
+    vim.fn.getpos("v"),
+    { type = "v" }
+  )[1]
+  vim.cmd.help(visual_selected)
+end, {
+  desc = "Help of selection"
+})
+
+-- Find man page for visual selected
+vim.keymap.set({ "v" }, "gm", function()
+  local visual_selected = vim.fn.getregion(
+    vim.fn.getpos("."),
+    vim.fn.getpos("v"),
+    { type = "v" }
+  )[1]
+  vim.cmd.Man(visual_selected)
+end, {
+  desc = "Man page of selection"
+})
